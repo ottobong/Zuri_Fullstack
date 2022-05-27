64caa6b24e6dbaf2fabd9abd01781f763571726f
Ottobong <clemency50@gmail.com>
2022-05-27 19:43:00 +0100

    added new files and renamed repo

---
+++ Word Counter.py
@@ -0,0 +1,10 @@
+sentence = "This is my first python program and I promise to be awesome"
+
+#original string
+print ("Our sentence for counting: " + sentence)
+
+# using split() function
+num = len(sentence.split())
+
+# total no of words
+print ("Your word count is: " + str(num))
